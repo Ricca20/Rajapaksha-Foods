b41f9c6207c774c362d153ccd530514bff3e9e16
yasindu-tech <166025746+Yasindu-Gmg@users.noreply.github.com>
2025-08-25 16:44:16 +0530

    setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from 'express';
-import connectDB from './db.js';
+import connectDB from './infastructure/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
