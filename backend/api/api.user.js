e28f4f22c6d248737677da5fac1d37e963aea558
yasindu-tech <166025746+Yasindu-Gmg@users.noreply.github.com>
2025-08-27 19:50:54 +0530

    refactor(UserRouter): remove unnecessary blank lines for cleaner code

--- backend/api/api.user.js
+++ backend/api/api.user.js
@@ -2,9 +2,7 @@ import express from 'express';
 import { handleWebhook } from '../application/application.user.js';
 
 const UserRouter = express.Router();
-
-
-
 UserRouter.post('/webhook', handleWebhook);
 
 export default UserRouter;
+
